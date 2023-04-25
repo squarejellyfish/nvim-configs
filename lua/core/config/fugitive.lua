af3410696667792a1343f79495a698675db88e62
jasonjustin <zxc3.zxc46@gmail.com>
2023-04-25 16:25:44 +0800

    added keybinds for fugitive

--- lua/core/config/fugitive.lua
+++ lua/core/config/fugitive.lua
@@ -2,4 +2,4 @@ local km = vim.keymap
 km.set("n", "<leader>gs", ":Git status<CR>")
 km.set("n", "<leader>ga", ":Git add ")
 km.set("n", "<leader>gc", ":Git commit<CR>")
-km.set("n", "<leader>gp", ":Git push<CR>")
+km.set("n", "<leader>gp", ":Git push origin master<CR>")
