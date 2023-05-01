20ef9079123679f0afc5080f70d87067c67d706b
jasonjustin <zxc3.zxc46@gmail.com>
2023-05-01 16:15:54 +0800

    ensure treesitter install cpp

--- lua/core/config/treesitter.lua
+++ lua/core/config/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup {
-    ensure_installed = {"c", "vim", "lua", "json", "python", "markdown" },
+    ensure_installed = {"c", "vim", "lua", "json", "python", "markdown", "cpp" },
 
     highlight = {
         enable = true,
